3ea831bdfeebede08401ba8696962f9a91763852
manu <manuthala7@gmail.com>
2024-12-15 08:47:47 +0530

    Added varities of code to solve Anagram probelm

--- OTHERS/Anagram.py
+++ OTHERS/Anagram.py
@@ -1,26 +1,58 @@
+# def anagram(s1,s2):
+#     if len(s1) != len(s2):
+#         return False
+    
+#     dict1={}
+#     dict2={}
+    
+#     for ch in s1:
+#         if ch in dict1:
+#             dict1[ch]+=1
+#         else:
+#             dict1[ch]=1
+
+#     for ch in s2:
+#         if ch in dict2:
+#             dict2[ch]+=1
+#         else:
+#             dict2[ch]=1
+
+#     for i in dict1:
+#         if i not in dict2 or dict1[i] != dict2[i]:
+#             return False
+#     return True
+
+# s1='Hello'
+# s2='olleH'
+# res=anagram(s1,s2)
+# if res==True:
+#     print('Anagram')
+# else:
+#     print('strings are not an anagram')
+
+# =============================================
+
+# Second type of code
+
+# def anagram(s1,s2):
+#     if len(s1) != len(s2):
+#         return False
+#     else:
+#         return sorted(s1) == sorted(s2)
+# s1='Hello'
+# s2='olleH'
+# res=anagram(s1,s2)
+# if res==True:
+#     print('Anagram')
+# else:
+#     print('strings are not an anagram')
+
+# Third type of code
+from collections import Counter ;
 def anagram(s1,s2):
     if len(s1) != len(s2):
         return False
-    
-    dict1={}
-    dict2={}
-    
-    for ch in s1:
-        if ch in dict1:
-            dict1[ch]+=1
-        else:
-            dict1[ch]=1
-
-    for ch in s2:
-        if ch in dict2:
-            dict2[ch]+=1
-        else:
-            dict2[ch]=1
-
-    for i in dict1:
-        if i not in dict2 or dict1[i] != dict2[i]:
-            return False
-    return True
+    return  Counter(s1)==Counter(s2)
 
 s1='Hello'
 s2='olleH'
