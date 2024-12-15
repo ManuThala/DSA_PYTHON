b6315fbc91f922e02b00388adc364d594bc10029
manu <manuthala7@gmail.com>
2024-12-15 08:39:38 +0530

    Modified code of anagram

--- OTHERS/Anagram.py
+++ OTHERS/Anagram.py
@@ -1,13 +1,31 @@
 def anagram(s1,s2):
     if len(s1) != len(s2):
         return False
-    if sorted(s1) == sorted(s2):
-        return True
+    
+    dict1={}
+    dict2={}
+    
+    for ch in s1:
+        if ch in dict1:
+            dict1[ch]+=1
+        else:
+            dict1[ch]=1
 
-s1='hello'
-s2='lloeh'
+    for ch in s2:
+        if ch in dict2:
+            dict2[ch]+=1
+        else:
+            dict2[ch]=1
+
+    for i in dict1:
+        if i not in dict2 or dict1[i] != dict2[i]:
+            return False
+    return True
+
+s1='Hello'
+s2='olleH'
 res=anagram(s1,s2)
 if res==True:
-    print('anagram')
+    print('Anagram')
 else:
-    print('not an anagram')
+    print('strings are not an anagram')
